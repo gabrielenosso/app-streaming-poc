38e9368ab822afc25e687faa7c4b785fe3d0970c
Gabriele Nosso <gabrielenosso@xrite.com>
2017-09-26 11:35:30 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# INSTALLATION
+Install the building tools (Python included) with `npm --add-python-to-path='true' --debug install --global windows-build-tools`.
+Then run `npm install`.
+
+
+Install ffpmeg: download it and add its `bin` folder to the PATH.
+Install https://github.com/rdp/screen-capture-recorder-to-video-windows-free.
+
+Share screen
+ffmpeg -f dshow -i video="screen-capture-recorder" output.flv
+
+Share single process
+ffmpeg -f gdigrab -framerate 30 -i title="german.avi - VLC media player" -b:v 3M  germ.flv
+where "title" means actual title of a target window.
---
+++ app.js
@@ -0,0 +1,63 @@
+import http from 'http';
+import express from 'express';
+import socketIo from 'socket.io';
+import socketIoStream from 'socket.io-stream';
+import fluentFfmpeg from 'fluent-ffmpeg';
+import base64 from 'base64-stream';
+
+// Constants
+const port = 3000;
+
+// fs.createReadStream('output.flv').pipe(stream);
+
+const app = express();
+const server = http.Server(app);
+const io = socketIo(server);
+
+server.listen(port);
+
+app.use(express.static('public'))
+
+app.get('/socket.io.js', function (req, res) {
+  res.sendFile(__dirname + '/node_modules/socket.io-client/dist/socket.io.js');
+});
+app.get('/socket.io-stream.js', function (req, res) {
+  res.sendFile(__dirname + '/node_modules/socket.io-stream/socket.io-stream.js');
+});
+app.get('/broadway-decoder.js', function (req, res) {
+  res.sendFile(__dirname + '/vendor/broadway/Decoder.js');
+});
+app.get('/broadway-player.js', function (req, res) {
+  res.sendFile(__dirname + '/vendor/broadway/Player.js');
+});
+
+io.on('connection', function (socket) {
+  // socket.on('executeCommand', function (data) {
+  //   console.log(data);
+  // });
+  //
+  // socket.emit('streamPicture', { hello: 'world' });
+
+  const stream = socketIoStream.createStream();
+  // socketIoStream.forceBase64 = true;
+  socketIoStream(socket).emit('streamVideo', stream);
+
+  const ffmpeg = fluentFfmpeg();
+  ffmpeg
+  .input('video=screen-capture-recorder')
+  .inputOptions([
+    '-f dshow'
+  ])
+  .on('error', function(err) {
+    console.log('An error occurred: ' + err.message);
+  })
+  .on('end', function() {
+    console.log('Streaming finished');
+  })
+  .format('flv')
+  .videoCodec('libx264')
+  .pipe(stream)
+  // .pipe(base64.encode()).pipe(stream);
+});
+
+console.log(`SERVER LISTENTING ON PORT: ${port}`)
